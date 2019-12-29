8ba8f6c5edc47f7fe056ae9259dd7270d09b1fcb
Luis Petrella <aax_00ap@hotmail.com>
2019-12-29 15:31:13 -0400

    Profile styling ready for own and non-own profile. Pending scroll style

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import ToolBar from '../components/ToolBar';
 import Navigation from '../components/Navigation';
 /**
@@ -7,54 +7,84 @@ import Navigation from '../components/Navigation';
  * @author Gabriel Trompiz (https://github.com/gabrieltrompiz)
  * @author Luis Petrella (https://github.com/Ptthappy)
  */
-const Profile: React.FC = () => {
+const Profile: React.FC<ProfileProps> = ({ user, own }) => {
+  const [name, setName] = useState(user.fullname);
+  const [username, setUsername] = useState(user.username);
+  const [email, setEmail] =  useState(user.email);
+  const [password, setPassword] =  useState(user.password);
 
   return (
     <div id='profile'>
       <div>
         <div>
           <div>Profile</div>
-          <div><img src={require('../assets/images/close.png')} alt='close'></img></div>
+          <div>
+            <img src={require('../assets/images/close.png')} alt='profile'></img>
+          </div>
         </div>
         <div>
-          <p>Profile Photo</p>
+          <p className="label">Profile Photo</p>
           <div>
             <div>
-              <img src={require('../assets/images/default-pic.png')} alt="picture"/>
+              {!user.imageUrl && <img src={require('../assets/images/default-pic.png')} alt="picture"/>}
+              {user.imageUrl && <img src={user.imageUrl} alt='profile'></img>}
             </div>
           
+            {own &&
             <div>
               <button>Change Profile Photo</button>
               <button>Delete Profile Photo</button>
-            </div>
+            </div>}
           </div>
           
-          <p>Name</p>
-          <input></input>
-          <p>Username</p>
-          <input></input>
-          <p>Email</p>
-          <input></input>
-          <p>Password</p>
-          <input type="password"></input>
+          {own &&
+          <Fragment>
+            <p className="label">Name</p>
+            <input value={name} onChange={(e) => setName(e.target.value)}></input>
+            <p className="label">Username</p>
+            <input value={username} onChange={(e) => setUsername(e.target.value)}></input>
+            <p className="label">Email</p>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+            <p className="label">Password</p>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password"></input>
+          </Fragment>}
+
+          {!own &&
+          <Fragment>
+            <p className="label">Name</p>
+            <p className="user-info">{user.fullname}</p>
+            <p className="label">Username</p>
+            <p className="user-info">{user.username}</p>
+            <p className="label">Email</p>
+            <p className="user-info">{user.email}</p>
+          </Fragment>}
           
-          <p>GitHub Account</p>
-          <div>
+          <p className="label">GitHub Account</p>
+          {!user.gitHubUser && own &&
+          <button id='gh-btn'>
+            <img src={require('../assets/images/github-color.png')} alt='gh-logo'></img>
+            Link GitHub Account
+          </button>}
+          {!user.gitHubUser && !own &&
+          <p className="user-info">This user has no linked GitHub Account</p>}
+          {user.gitHubUser &&
+          <div id="gh-info">
             <div>
               <img src={require('../assets/images/default-pic.png')} alt="github"/>
             </div>
             <div>
-              <span>@gabtrompiz</span>
+              <span>@{user.gitHubUser.username}</span>
               <span>Connected</span>
             </div>
             <div>
-              <button>Unlink</button>
+              {own && <button>Unlink</button>}
             </div>
-          </div>
+          </div>}
           
+          {own &&
           <div>
             <button>Save Changes</button>
-          </div>
+          </div>}
         </div>
       </div>
     </div>
@@ -62,3 +92,8 @@ const Profile: React.FC = () => {
 };
 
 export default Profile;
+
+interface ProfileProps {
+  user: any;
+  own: boolean;
+}
