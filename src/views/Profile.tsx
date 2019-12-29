6f01cc5873d4a600ce2b66c3ac6fd6b76a94ef23
Gabriel Trompiz <gab.tc@icloud.com>
2019-12-29 17:29:53 -0400

    finished styling of profile

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -19,73 +19,75 @@ const Profile: React.FC<ProfileProps> = ({ loggedUser, user = loggedUser }) => {
     <div id='profile'>
       <div>
         <div>
-          <div>Profile</div>
           <div>
-            <img src={require('../assets/images/close.png')} alt='profile'></img>
-          </div>
-        </div>
-        <div>
-          <p className="label">Profile Photo</p>
-          <div>
+            <div>Profile</div>
             <div>
-              <img src={user.pictureUrl} alt='profile'></img>
+              <img src={require('../assets/images/close.png')} alt='profile'></img>
             </div>
-          
+          </div>
+          <div>
+            <p className="label">Profile Photo</p>
+            <div>
+              <div>
+                <img src={user.pictureUrl} alt='profile'></img>
+              </div>
+            
+              {own &&
+              <div>
+                <button>Change Profile Photo</button>
+                <button>Delete Profile Photo</button>
+              </div>}
+            </div>
+            
+            {own &&
+            <Fragment>
+              <p className="label">Name</p>
+              <input value={name} onChange={(e) => setName(e.target.value)}></input>
+              <p className="label">Username</p>
+              <input value={username} onChange={(e) => setUsername(e.target.value)}></input>
+              <p className="label">Email</p>
+              <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+              <p className="label">Password</p>
+              <input value={password} onChange={(e) => setPassword(e.target.value)} type="password"></input>
+            </Fragment>}
+
+            {!own &&
+            <Fragment>
+              <p className="label">Name</p>
+              <p className="user-info">{user.fullName}</p>
+              <p className="label">Username</p>
+              <p className="user-info">{user.username}</p>
+              <p className="label">Email</p>
+              <p className="user-info">{user.email}</p>
+            </Fragment>}
+            
+            <p className="label">GitHub Account</p>
+            {!user.gitHubUser && own &&
+            <button id='gh-btn'>
+              <img src={require('../assets/images/github-color.png')} alt='gh-logo'></img>
+              Link GitHub Account
+            </button>}
+            {!user.gitHubUser && !own &&
+            <p className="user-info">This user has no linked GitHub account</p>}
+            {user.gitHubUser &&
+            <div id="gh-info">
+              <div>
+                <img src={user.gitHubUser.avatarUrl} alt="github"/>
+              </div>
+              <div>
+                <span>@{user.gitHubUser.login}</span>
+                <span>Connected</span>
+              </div>
+              <div>
+                {own && <button>Unlink</button>}
+              </div>
+            </div>}
+            
             {own &&
             <div>
-              <button>Change Profile Photo</button>
-              <button>Delete Profile Photo</button>
+              <button>Save Changes</button>
             </div>}
           </div>
-          
-          {own &&
-          <Fragment>
-            <p className="label">Name</p>
-            <input value={name} onChange={(e) => setName(e.target.value)}></input>
-            <p className="label">Username</p>
-            <input value={username} onChange={(e) => setUsername(e.target.value)}></input>
-            <p className="label">Email</p>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
-            <p className="label">Password</p>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password"></input>
-          </Fragment>}
-
-          {!own &&
-          <Fragment>
-            <p className="label">Name</p>
-            <p className="user-info">{user.fullName}</p>
-            <p className="label">Username</p>
-            <p className="user-info">{user.username}</p>
-            <p className="label">Email</p>
-            <p className="user-info">{user.email}</p>
-          </Fragment>}
-          
-          <p className="label">GitHub Account</p>
-          {!user.gitHubUser && own &&
-          <button id='gh-btn'>
-            <img src={require('../assets/images/github-color.png')} alt='gh-logo'></img>
-            Link GitHub Account
-          </button>}
-          {!user.gitHubUser && !own &&
-          <p className="user-info">This user has no linked GitHub account</p>}
-          {user.gitHubUser &&
-          <div id="gh-info">
-            <div>
-              <img src={user.gitHubUser.avatarUrl} alt="github"/>
-            </div>
-            <div>
-              <span>@{user.gitHubUser.login}</span>
-              <span>Connected</span>
-            </div>
-            <div>
-              {own && <button>Unlink</button>}
-            </div>
-          </div>}
-          
-          {own &&
-          <div>
-            <button>Save Changes</button>
-          </div>}
         </div>
       </div>
     </div>
